77f6980b96b47896c7a902b3d83360185737873e
Khotso <Mokhetkc@hotmail.com>
2023-06-07 17:42:50 +0200

    View Child - Learn the multiple View Query Configuration Options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './models/course';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -10,12 +10,15 @@ import {CourseCardComponent} from './course-card/course-card.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('cardRef1', {read: ElementRef}) card1: CourseCardComponent;
+  @ViewChild('cardRef2') card2: CourseCardComponent;
+  @ViewChild('container') containerDiv: ElementRef;
+
   courses = COURSES;
-  startDate = new Date(2000,0,1);
+  startDate = new Date(2000, 0, 1);
   title = COURSES[1].description;
-  price= 3.66;
-  @ViewChild(CourseCardComponent)
-  card:CourseCardComponent;
+  price = 3.66;
+
 
   course = COURSES[1];
   // public coreCourses = COURSES[0];
@@ -23,8 +26,10 @@ export class AppComponent {
   // public ngRxCourses = COURSES[2];
 
   public onCourseSelected(course: Course) {
-    //console.log('On Course selected event triggered from app',course);
-    console.log(this.card);
+    // console.log('On Course selected event triggered from app',course);
+    console.log(this.card1);
+    // console.log(this.card2);
+   // console.log('containerDiv', this.containerDiv);
   }
 
   isImageVisible() {
